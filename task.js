90bc22114f19bb8c9b72f6454f122792e40dec35
priyanshupandey12 <129362864+priyanshupandey12@users.noreply.github.com>
2024-07-14 12:37:49 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,38 @@
+
+//activity-1:Arthimetic operations
+
+let a=4,b=3;
+console.log(`the sum is ${a+b}`)
+console.log(`the subtract is ${a-b}`)
+console.log(`the mul is ${a*b}`)
+console.log(`the divide is ${a/b}`)
+console.log(`the remainder is ${a%b}`)
+
+//activity-2:Assignment operation
+let c=3,d=4;
+c+=c;
+d-=d;
+console.log(`the  value of c is ${c} && the value of d is ${d} `);
+
+//activity-3:Comparison operation
+
+
+  console.log(`the  value of comaparison is ${c<d}`);
+
+
+  console.log(`the value of comparisom is ${d>c} `);
+
+
+let e=4;
+let f='4';
+console.log(`comparison between e and f == ${e==f}`)
+console.log(`comparison between e and f === ${e===f}`)
+
+//activity-4:logical operation
+console.log(`the  value of comaparison of ANd ${c<d && d==0}`);
+console.log(`the  value of comaparison of OR ${c<d || d==0}`);
+console.log(`the value of comparisom of not opeartor ${!(d>c)} `);
+
+//activity-5:ternary opeartor
+
+e<0?console.log('number is negative'):console.log('positive number')
